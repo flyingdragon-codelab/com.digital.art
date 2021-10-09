34a2d394d748cde24394e3402a4ba15c4bdcf7e6
stevensoe0524 <80680919+stevensoe0524@users.noreply.github.com>
2021-10-09 11:06:26 +0800

    Creating project

---
+++ android/app/src/main/kotlin/com/digital/digital_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.digital.digital_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,240 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+//  await Firebase.initializeApp();
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage()
+  )
+  );
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width=MediaQuery.of(context).size.width;
+    double height=MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo Login UI'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Container(
+          height: height,
+          width: width,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: width,
+                  height: height*0.45,
+                  child: Image.asset('assets/yoga.png',fit: BoxFit.fill,),
+                ),
+                Container(
+                    width: width * 0.8,
+                    child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text('Login',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 30.0,),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              suffixIcon: Icon(Icons.email),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20.0,),
+                          TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              suffixIcon: Icon(Icons.visibility_off),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30.0,),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Forget password?',style: TextStyle(fontSize: 12.0),),
+                                RaisedButton(
+                                  child: Text('Login'),
+                                  color: Color(0xffEE7B23),
+                                  onPressed: (){},
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height:20.0),
+                          GestureDetector(
+                            onTap: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>Second()));
+                            },
+                            child: Text.rich(
+                              TextSpan(
+                                  text: 'Don\'t have an account ',
+                                  children: [
+                                    TextSpan(
+                                      text: 'Signup',
+                                      style: TextStyle(
+                                          color: Color(0xffEE7B23)
+                                      ),
+                                    ),
+                                  ]
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 200,
+                          ),
+                        ]
+                    )
+                )
+              ],
+            ),
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+class Second extends StatefulWidget {
+  @override
+  _SecondState createState() => _SecondState();
+}
+
+class _SecondState extends State<Second> {
+  @override
+  Widget build(BuildContext context) {
+    double width=MediaQuery.of(context).size.width;
+    double height=MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: width,
+                height: height*0.45,
+                child: Image.asset('assets/play.png',fit: BoxFit.fill,),
+              ),
+              Container(
+                  width: width * 0.8,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text('Signup',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 30.0,),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          suffixIcon: Icon(Icons.email),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0,),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          suffixIcon: Icon(Icons.visibility_off),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0,),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Forget password?',style: TextStyle(fontSize: 12.0),),
+                            RaisedButton(
+                              child: Text('Signup'),
+                              color: Color(0xffEE7B23),
+                              onPressed: (){},
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height:20.0),
+                      GestureDetector(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>MyHomePage()));
+                        },
+                        child: Text.rich(
+                          TextSpan(
+                              text: 'Already have an account ',
+                              children: [
+                                TextSpan(
+                                  text: 'Signin',
+                                  style: TextStyle(
+                                      color: Color(0xffEE7B23)
+                                  ),
+                                ),
+                              ]
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
