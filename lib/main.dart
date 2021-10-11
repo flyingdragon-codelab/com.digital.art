9772517eb15488ac09bb8377d108c3c4f30a6201
stevensoe0524 <80680919+stevensoe0524@users.noreply.github.com>
2021-10-11 16:26:50 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,13 @@ class LandingPageState extends State<LandingPage> {
     });
   }
 
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     User? user = FirebaseAuth.instance.currentUser;
@@ -145,6 +152,25 @@ class LandingPageState extends State<LandingPage> {
               ],
             ),
           )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
